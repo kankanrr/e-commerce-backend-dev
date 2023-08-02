3e8493a0b825ce81f8883ca34618a9f7a7081263
Sameer Mirza <srmirza14@gmail.com>
2023-08-01 23:20:54 -0400

    fixed some product errs

--- seeds/tag-seeds.js
+++ seeds/tag-seeds.js
@@ -1,29 +1,32 @@
-const { Tag } = require('../models');
+const { Tag } = require("../models");
 
 const tagData = [
   {
-    tag_name: 'underground rap',
+    tag_name: "underground rap",
   },
   {
-    tag_name: 'hip hop',
+    tag_name: "hip hop",
   },
   {
-    tag_name: 'blue',
+    tag_name: "blue",
   },
   {
-    tag_name: 'red',
+    tag_name: "red",
   },
   {
-    tag_name: 'green',
+    tag_name: "white",
   },
   {
-    tag_name: 'white',
+    tag_name: "software",
   },
   {
-    tag_name: 'gold',
+    tag_name: "pop culture",
   },
   {
-    tag_name: 'pop culture',
+    tag_name: "toy",
+  },
+  {
+    tag_name: "collectible",
   },
 ];
 
